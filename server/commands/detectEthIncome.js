b6e61ef60d4a1c96454e5f6b52a6fe7e3d6d288e
Twanaar <alain.saffray@gmail.com>
2018-12-04 11:56:09 -0300

    uncommit test code

--- server/commands/detectEthIncome.js
+++ server/commands/detectEthIncome.js
@@ -497,12 +497,10 @@ DetectEthereumIncome.prototype.Init = function (cb, checkMode) {
 				.query({convert: 'EUR'})
 				.timeout(5000)
                 .end((err, res) => {
-                    var lastGetCot = null;
-                    pingAlive(lastGetCot);
                     if (err) return cb(err, null);
                     if (res.body && !res.error && res.statusCode===200 && res.text && res.text.length>0) {
                         lastGetCot = JSON.parse(res.text);
-
+                        pingAlive(lastGetCot);
                         return cb(null, lastGetCot);
                     } else {
                         return cb('request() error. url:' + url, null);
